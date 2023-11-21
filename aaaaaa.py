b18c5f024faf32aa728c1d2d3a8bf5cf05be904f
Mehdy Mimouni <mimouni.mehdy@gmail.com>
2023-11-21 14:14:36 +0100

    test

---
+++ aaaaaa.py
@@ -0,0 +1,32 @@
+import subprocess
+import json
+
+class Repository():
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.branches = []
+
+    def get_branches(self):
+        command = ["gh", "repo", "view", "--json=branches", self.name]
+        print(" ".join(command))
+
+        try:
+            result = subprocess.run(command, stdout=subprocess.PIPE, text=True, check=True)
+            repo_data = json.loads(result.stdout)
+            branches_data = repo_data['branches']
+
+            for branch_data in branches_data:
+                branch_name = branch_data['name']
+                self.branches.append(branch_name)
+        except subprocess.CalledProcessError as e:
+            print(f"Erreur : {e}")
+
+# Exemple d'utilisation avec un répertoire spécifique
+repository_name = 'NomRepo1'  # Remplacez par le nom du répertoire souhaité
+repository_url = 'https://github.com/proprietaire1/NomRepo1'  # Remplacez par l'URL du répertoire
+
+repo = Repository(repository_name, repository_url)
+repo.get_branches()
+
+print("Noms des branches :", repo.branches)
